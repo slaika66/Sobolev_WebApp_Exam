9048e5eb29419623ebebc9675ae4837fca4519c7
slaika66 <146094473+slaika66@users.noreply.github.com>
2025-06-16 08:21:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -636,5 +636,6 @@ if __name__ == '__main__':
         db.session.commit()
     app.run(debug=True)
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
